6501d6a5a5a1a0b1d666e31d2160f36acd730a27
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-13 18:13:51 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
 *main - prints to the standard error
 *
-*Return: returns a 0 value
+*Return: returns a 1 value
 */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", "string format", 60);
-	return (0);
+	return (1);
 }
