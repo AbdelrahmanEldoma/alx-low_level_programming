3a3a2760ab1646f04e320c8ce205e3dbdfd2dbb6
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-13 18:36:51 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	char str[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	 fprintf(stderr, "%s", str);
+	char s[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	 fprintf(stderr, "%s", s);
 	return (1);
 }
