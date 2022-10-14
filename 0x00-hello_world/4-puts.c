13cfbc5e9a2735091bbe5c5450372cd28f0c0fa3
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-13 17:10:00 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - prints a string
+*
+*Return: a 0 value
+*/
+int main(void)
+{
+	char str1[20] = "\"Programming is like building a multilingual puzzle";
+	 puts(str1);
+	return (0);
+
+}
