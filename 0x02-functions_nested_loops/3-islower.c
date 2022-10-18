24bd5c5d6adc2795c5e9254c731c1541fba7e309
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-18 11:20:55 -0700

    C programming

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-int _islower(int c);
-/**
-*main - calls another function
-*
-*Return: returns a 0 value
-*/
-int main(void)
-{
-	int r;
-
-        r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
 /**
 *_islower - checks if a character is lower case
 *
