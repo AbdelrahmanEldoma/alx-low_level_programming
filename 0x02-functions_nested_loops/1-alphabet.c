724b686cfc15129fa19382cc90ed932fc605dab6
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-18 11:03:50 -0700

    C programming

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
-#include "main.h"
 void print_alphabet(void);
-/**
-*main - class another function
-*
-*Return: returns a 0 value
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 /**
 *print_alphabet - prints thr alphabet
 *
