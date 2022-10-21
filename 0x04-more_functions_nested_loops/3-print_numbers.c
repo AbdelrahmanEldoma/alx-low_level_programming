72870f952445a7d9b9134797c963489e4b2cf5d3
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-21 16:50:02 -0700

    C programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers- prints numbers from 0 to 9
+*
+*Return: the value of multiplication
+*/
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; ++i)
+{
+	printf("%d",i);
+}
+	printf("\n");
+}
