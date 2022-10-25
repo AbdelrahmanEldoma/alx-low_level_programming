ab7e0988ded25dd684037c3d10a9fb04009c2e97
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-24 17:09:28 -0700

    C programming

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*_puts-prints a string, followed by a new line, to stdout
+*
+*@str: a string
+*Return: a void
+*/
+
+void _puts(char *str)
+
+{
+	puts(str);
+}
