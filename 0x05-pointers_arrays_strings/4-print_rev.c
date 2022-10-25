4bc349a0ed360e77af9786b8c7403b8073efcf20
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-24 17:53:17 -0700

    C programming

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,16 @@
 
 void print_rev(char *s)
 {
-	rev_string(s);
+	int i = strlen(s) - 1, j = 0;
+	char ch;
+
+	while (i > j)
+{
+	ch = s[i];
+	s[i] = s[j];
+	s[j] = ch;
+	i--;
+	j++;
+}
 	puts(s);
 }
