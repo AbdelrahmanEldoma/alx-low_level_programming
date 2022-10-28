9f2e19ac4a8597e77e250fe94cf1952e75da46e9
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-28 12:48:50 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
+	for (i = 0; i < n; i++)
+{        
 	if (n == 0)
 {
 	break;
 }
-	for (i = 0; i < n; i++)
-{
 	dest[i] = src[i];
 }
 	return (dest);
