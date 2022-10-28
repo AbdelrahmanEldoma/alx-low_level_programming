f507169d62a658258c4f15c572435c8f90d7dea7
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-28 12:18:55 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,5 +10,18 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	return (memset(strcat(dest, src), 0, n));
+	int i1 = 0;
+	int i2 = 0;
+
+	while (dest[i1] != '\0')
+{
+	i1++;
+}
+	while (i2 < n && src[i2] != '\0')
+
+	dest[i1 + i2] = src[i2];
+	i2++;
+}
+	dest[i1 + i2] = '\0';
+	return (dest);
 }
