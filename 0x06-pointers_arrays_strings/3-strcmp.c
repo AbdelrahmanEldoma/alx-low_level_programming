55c4c89a73563845a84acc368c11a1bdca1dd734
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-10-27 19:10:30 -0700

    alx-low_level_programming

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <string.h>
+/**
+*_strcmp - concatenates two strings
+*
+*@s1: a string
+*@s1: a string
+*Return: an integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
