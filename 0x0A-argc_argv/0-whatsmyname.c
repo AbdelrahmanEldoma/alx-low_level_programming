7a92eb374fd9544d9d9f3d39e86b6da1e2afb4fa
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-11-07 15:49:09 -0800

    C programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *@argv: the command line variables
  *Return: a zero
  */
-int main(int __attribute__((__unused__)) int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
