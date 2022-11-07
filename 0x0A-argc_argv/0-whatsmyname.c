140717f1f8c9cc1921f648f3daf62942ba33997f
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-11-07 14:50:09 -0800

    C programming

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints program name
+ *@argc: the length of argv
+ *@argv: the command line variables
+ *Return: a zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+0-whatsmyname.c
+1-args.c
+2-args.c
+3-mul.c
+4-add.c
+100-change.c
+
+
+#endif
