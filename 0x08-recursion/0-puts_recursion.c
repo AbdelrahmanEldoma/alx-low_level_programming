02cd4ebfa7699642281b39493ce96e577e8263a8
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-11-25 20:16:24 -0800

    C programming

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+*Myrec - the function used for recursion
+*@s: a string
+*@x: an intger
+*/
+
+void Myrec(char *s, int x)
+{
+	if (s[x] == '\0')
+	return;
+
+	printf("%c", s[x]);
+	Myrec(s, x + 1);
+}
+
 /**
 *_puts_recursion - prints string with recursion
 *@s: a string
 */
-
 	void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-{
-	return;
-}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	Myrec(s, 0);
 }
