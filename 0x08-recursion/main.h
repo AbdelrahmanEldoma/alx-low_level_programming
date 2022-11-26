f7f1cc1d577ada580e947499a98890921d9c9e36
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-11-25 19:42:15 -0800

    C programming

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 #include <stddef.h>
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+void Myrec (char *s, int x);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
