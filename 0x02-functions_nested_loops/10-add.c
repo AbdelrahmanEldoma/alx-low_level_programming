59c5b177f38f6bc9af71c5cc70111dd6ec43991b
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-12-11 07:55:07 -0800

    C programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+int add(int, int);
+/*
+ *add - adds two numbers
+ *
+ *@x: a number
+ *@y: a number
+ *
+ *return : sum of the two numbers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
