5bef0725c4f18b26016e8df76d9cca2fd00b1217
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2022-12-11 04:54:11 -0800

    C programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int j, i;
 
-	for (int i = 0; i < 10; ++i)
+	for (i = 0; i < 10; ++i)
 {
-	for (int j = 0; j < 10; ++j)
+	for (j = 0; j < 10; ++j)
 {
 	if (j != 9)
 	printf("%d, ", i * j);
