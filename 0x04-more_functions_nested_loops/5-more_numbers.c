722664a24523413aeaff7150fda3f25260fc19e4
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-23 03:02:44 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*more_numbers- prints numbers from 0 to 14
+*
+*Return: no return value
+*/
+void more_numbers(void)
+{
+	int i;
+
+	for (int i = 0; i < 11; ++i)
+{
+	int i;
+
+	for (int i = 0; i < 15; ++i)
+{
+	printf("%d", i);
+}
+	printf("\n");
+}
+}
+
