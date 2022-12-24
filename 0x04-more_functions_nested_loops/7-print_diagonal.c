a498f792b5f63fc9b6c7add7fed5c97673eea13d
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-24 18:50:04 +0300

    C programming

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,8 @@
 */
 void print_diagonal(int n)
 {
+	if (n > 0)
+{
 	int i;
 
 	for (i = 0; i < n; ++i)
@@ -19,5 +21,8 @@ void print_diagonal(int n)
 	putchar(92);
 	putchar('\n');
 }
+}
+	else
+		putchar('\n');
 }
 
