a8ffc302ad715601514614fdedd400847c8f6ffd
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-24 22:17:20 +0300

    C programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,19 +15,15 @@ int  main(void)
 	for (i = 1; i <= 100; ++i)
 {
 	if (i % 3 == 0 && i % 5 != 0)
-{
 	printf("Fizz ");
-}
 	else if (i % 3 != 0 && i % 5 == 0)
-{
 	printf("Buzz ");
-}
 	else if (i % 3 == 0 && i % 5 == 0)
-{
 	printf("FizzBuzz ");
-}
-	else if (i % 3 != 0 && i % 5 != 0)
-		printf("%d ", i);
+	else if(i % 3 != 0 && i % 5 != 0 && i != 100)
+	printf("%d ", i);
+	else if(i % 3 != 0 && i % 5 != 0 && i == 100)
+	printf("%d", i);
 }
 	putchar('\n');
 	return (0);
