8bbf1547ff18f05edc854c82da9afb6fad5b7f15
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-24 18:45:03 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_diagonal- prints "\" n times
+*
+*@n: an integer
+*Return: no return value
+*/
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+{
+	for (int j = 0; j < i; ++j)
+{
+	putchar(' ');
+}
+	putchar(92);
+	putchar('\n');
+}
+	putchar('\n');
+}
+
