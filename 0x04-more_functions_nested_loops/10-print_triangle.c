a734ed91ca37a6e1fdb608fdff8e63a5a5abce9e
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-24 22:46:54 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_diagonal- prints a tingle of "#"
+*
+*@size: an integer
+*Return: no return value
+*/
+void print_triangle(int size)
+{
+	if (size > 0)
+{
+	int i;
+	int j;
+	int a = size;
+
+	for (i = 0; i < size; ++i)
+{
+	for (j = 1; j < a; ++j)
+{
+	putchar(' ');
+}
+	for (j = -1; j < i; ++j)
+{
+	putchar('#');
+}
+	a--;
+	putchar('\n');
+}
+}
+	else
+	putchar('\n');
+}
