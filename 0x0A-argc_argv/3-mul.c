878e90a62b5e752d70b753f2d2da5908f49b97b0
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-25 22:21:11 +0300

    C programming

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - will multiply numbers.
+ * @argc: number of arguments.
+ * @argv: array the number.
+ * Return: return 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+{
+		printf("Error\n");
+		return (1);
+}
+	else
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - will add numbers.
+ * @argc: number of arguments.
+ * @argv: array the number.
+ * Return: return 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+{
+	for (j = 0; argv[i][j] != '\0'; j++)
+{
+	if (!isdigit(argv[i][j]))
+{
+	printf("Error\n");
+	return (1);
+}
+}
+	sum = sum + atoi(argv[i]);
+}
+	printf("%d\n", add);
+	return (0);
+}
