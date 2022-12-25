6243d4a4a4d547e9b5c1bbac8019084fc861b94c
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-25 21:48:25 +0300

    C programming

--- 0x0A-argc_argv/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
-#define ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
-
-int _putchar(char c);
-int main(int __attribute__((__unused__)) argc, char *argv[])
-int main(int argc, char *argv[] __attribute__((unused)))
-int main(int argc, char *argv[])
-
-#endif //ALX_LOW_LEVEL_PROGRAMMING_MAIN_H
