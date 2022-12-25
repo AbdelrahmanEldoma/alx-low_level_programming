5d3256869aab8df346c50272180dc938e0794ee8
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-25 21:32:50 +0300

    C programming

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,14 +9,8 @@
  * Return: return 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i = 0;
-
-	while (i < argc)
-{
-	printf("No of Arguments: %d, Name: %s\n", argc, argv[i]);
-	i++;
-}
+	printf("%d\n", argc - 1);
 	return (0);
 }
