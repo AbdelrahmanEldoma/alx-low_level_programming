8edb95e95b1ea0f41dc172e5aa7d7aae953f4fa7
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-26 20:17:48 +0300

    C programming

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,16 @@
 *@n: the number of bytes
 *@src: the memory area to copy from
 *
-* Return: a pointer to @dest
+* Return: a pointer
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
+{
 	dest[i] = src[i];
-
+}
 	return (dest);
 }
