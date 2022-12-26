5d6c34cf1f5f389ad0484853c98af006d1c9ba32
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-26 19:03:23 +0300

    C programming

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_print_rev_recursion - the function used for recursion
+*@s: a string
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+{
+	return;
+}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
