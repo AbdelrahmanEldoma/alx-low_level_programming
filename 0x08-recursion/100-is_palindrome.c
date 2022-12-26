a776d84a6fa9a08f0305ac5d095a13e1d2f58c74
AbdelrahmanEldoma <113690967+AbdelrahmanEldoma@users.noreply.github.com>
2022-12-26 20:00:12 +0300

    C programming

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+*is_palindrome-  checks if is palindrome
+*@s: int type
+*Return: char type
+**/
+int is_palindrome(char *s)
+{
+	int first, last;
+
+	first = 0;
+	last = _strlen_recursion(s) - 1;
+	return (palindrome(s, first, last));
+}
+
+/**
+*palindrome- used of check
+*@s: string
+*@first: a number
+*@last: a number
+*Return: the value
+**/
+int palindrome(char *s, int first, int last)
+{
+	if (first > last)
+{
+	return (1);
+}
+	else if (s[first] == s[last])
+{
+	return (palindrome(s, first + 1, last - 1));
+}
+	else
+	return (0);
+}
+#include "main.h"
+/**
+*_strlen_recursion- the function used for recursion
+*@s: a string
+*
+*Return: length of string or zero
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
