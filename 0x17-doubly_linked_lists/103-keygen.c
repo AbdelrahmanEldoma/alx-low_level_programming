5479a676bd051b907268d3720dcc4170e741ae26
AbdelrahmanEldoma <abdelrahman.eldoma@outlook.com>
2023-01-28 21:48:16 -0800

    C programming

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,11 +2,13 @@
 #include "lists.h"
 
 /**
- * keygen print a valid key for the username
- * Return: key of the username
- **/
+*  main - main function
+* keygen print a valid key for the username
+* Return: key of the username
+**/
 
-int main()
+int main(void)
 {
 	return (key);
 }
+
